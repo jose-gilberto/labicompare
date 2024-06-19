23da0bc28f95d0ed2e2cf176e3e263d64abdab3a
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2024-06-19 00:34:40 -0300

    feat: one_vs_one plot notebook

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project_name',
+    name='labicompare',
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
