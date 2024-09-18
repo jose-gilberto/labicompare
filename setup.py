f531fb876692343171a41231d66cba21719e7f98
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2024-09-18 15:29:32 -0300

    feat: setup config

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='labicompare',
-    version='0.1.0',
-    packages=find_packages(where='labicompare'),
-    package_dir={'': 'labicompare'},
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'matplotlib',
-        'seaborn',
-        'pyyaml',
-    ],
-)
+setup()
