3142221f3d18ad2c00c5b7a4b91501165e146fd4
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2024-09-24 14:00:00 -0300

    docs: update docs for bonferroni critical difference diagram

--- labicompare/stats/_bonferroni.py
+++ labicompare/stats/_bonferroni.py
@@ -2,7 +2,21 @@ import pandas as pd
 
 
 def bonferroni_dunn(avg_ranks: pd.Series, n: int, alpha: float = 0.05) -> float:
-    """ TODO: write docs for that function
+    """
+    Returns the critical difference for Bonferroni-Dunn according to a given alpha
+    (either = 0.1 or 0.05) for the average ranking of N datasets.
+
+    Args:
+        avg_ranks (pd.Series): average ranking of the models
+        n (int): number of datasets compared
+        alpha (float): alpha level. must be either 0.05 or 0.1
+
+    Returns:
+        cd (float): critical difference value.
+
+    Refs:
+        [1] Dunn, O. J. (1961). Multiple Comparisons among Means. Journal of the American Statistical Association, 56(293), 52–64.
+        [2] Demšar, Janez. "Statistical comparisons of classifiers over multiple data sets." The Journal of Machine learning research 7 (2006): 1-30.
     """
     ranks = avg_ranks.values
     k = ranks if isinstance(ranks, int) else len(ranks)
